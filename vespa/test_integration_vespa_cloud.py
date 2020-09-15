ee21eeaecd5247f591712285c884f816c13cb7cf
tmartins <thigm85@gmail.com>
2020-09-15 13:39:02 +0200

    create vespa cloud integration test

---
+++ vespa/test_integration_vespa_cloud.py
@@ -0,0 +1,66 @@
+import unittest
+import os
+import shutil
+from vespa.application import Vespa
+from vespa.package import (
+    Document,
+    Field,
+    Schema,
+    FieldSet,
+    RankProfile,
+    ApplicationPackage,
+    VespaCloud,
+)
+
+
+class TestDockerDeployment(unittest.TestCase):
+    def setUp(self) -> None:
+        #
+        # Create application package
+        #
+        document = Document(
+            fields=[
+                Field(name="id", type="string", indexing=["attribute", "summary"]),
+                Field(
+                    name="title",
+                    type="string",
+                    indexing=["index", "summary"],
+                    index="enable-bm25",
+                ),
+                Field(
+                    name="body",
+                    type="string",
+                    indexing=["index", "summary"],
+                    index="enable-bm25",
+                ),
+            ]
+        )
+        msmarco_schema = Schema(
+            name="msmarco",
+            document=document,
+            fieldsets=[FieldSet(name="default", fields=["title", "body"])],
+            rank_profiles=[
+                RankProfile(name="default", first_phase="nativeRank(title, body)")
+            ],
+        )
+        app_package = ApplicationPackage(name="msmarco", schema=msmarco_schema)
+        #
+        # Deploy in a Docker container
+        #
+        self.vespa_cloud = VespaCloud(
+            tenant="vespa-team",
+            application="pyvespa-integration",
+            key_location=os.getenv("VESPA_CLOUD_APP_KEY"),
+            application_package=app_package,
+        )
+        self.disk_folder = os.path.join(os.getenv("WORK_DIR"), "sample_application")
+        self.app = self.vespa_cloud.deploy(
+            instance="integration-test", disk_folder=self.disk_folder
+        )
+
+    def test_deployment_message(self):
+        self.assertIsInstance(self.app, Vespa)
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.disk_folder, ignore_errors=True)
+        self.vespa_cloud.delete(instance="integration-test")
