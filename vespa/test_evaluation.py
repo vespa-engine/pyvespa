8835875be8b7a84c28a6474448a9abb69e99b1d5
tmartins <thigm85@gmail.com>
2020-09-28 13:15:56 +0200

    add new test case containing scores > 1

--- vespa/test_evaluation.py
+++ vespa/test_evaluation.py
@@ -64,6 +64,68 @@ class TestEvalMetric(unittest.TestCase):
             }
         }
 
+        self.labelled_data2 = [
+            {
+                "query_id": 0,
+                "query": "Intrauterine virus infections and congenital heart disease",
+                "relevant_docs": [{"id": "ghi", "score": 1}, {"id": "abc", "score": 2}],
+            },
+        ]
+
+        self.query_results2 = {
+            "root": {
+                "id": "toplevel",
+                "relevance": 1.0,
+                "fields": {"totalCount": 1083},
+                "coverage": {
+                    "coverage": 100,
+                    "documents": 62529,
+                    "full": True,
+                    "nodes": 2,
+                    "results": 1,
+                    "resultsFull": 1,
+                },
+                "children": [
+                    {
+                        "id": "id:covid-19:doc::40216",
+                        "relevance": 10,
+                        "source": "content",
+                        "fields": {
+                            "vespa_id_field": "ghi",
+                            "sddocname": "doc",
+                            "body_text": "this is a body 2",
+                            "title": "this is a title 2",
+                            "rankfeatures": {"a": 3, "b": 4},
+                        },
+                    },
+                    {
+                        "id": "id:covid-19:doc::40217",
+                        "relevance": 8,
+                        "source": "content",
+                        "fields": {
+                            "vespa_id_field": "def",
+                            "sddocname": "doc",
+                            "body_text": "this is a body 3",
+                            "title": "this is a title 3",
+                            "rankfeatures": {"a": 5, "b": 6},
+                        },
+                    },
+                    {
+                        "id": "id:covid-19:doc::40218",
+                        "relevance": 6,
+                        "source": "content",
+                        "fields": {
+                            "vespa_id_field": "abc",
+                            "sddocname": "doc",
+                            "body_text": "this is a body 4",
+                            "title": "this is a title 4",
+                            "rankfeatures": {"a": 7, "b": 8},
+                        },
+                    },
+                ],
+            }
+        }
+
     def test_match_ratio(self):
         metric = MatchRatio()
 
@@ -174,6 +236,20 @@ class TestEvalMetric(unittest.TestCase):
             },
         )
 
+        metric = Recall(at=3)
+        evaluation = metric.evaluate_query(
+            query_results=VespaResult(self.query_results2),
+            relevant_docs=self.labelled_data2[0]["relevant_docs"],
+            id_field="vespa_id_field",
+            default_score=0,
+        )
+        self.assertDictEqual(
+            evaluation,
+            {
+                "recall_3_value": 1,
+            },
+        )
+
     def test_reciprocal_rank(self):
         metric = ReciprocalRank(at=2)
         evaluation = metric.evaluate_query(
@@ -200,6 +276,20 @@ class TestEvalMetric(unittest.TestCase):
             evaluation,
             {
                 "reciprocal_rank_1_value": 0.0,
+            },
+        )
+
+        metric = ReciprocalRank(at=3)
+        evaluation = metric.evaluate_query(
+            query_results=VespaResult(self.query_results2),
+            relevant_docs=self.labelled_data2[0]["relevant_docs"],
+            id_field="vespa_id_field",
+            default_score=0,
+        )
+        self.assertDictEqual(
+            evaluation,
+            {
+                "reciprocal_rank_3_value": 1.0,
             },
         )
 
@@ -240,5 +330,24 @@ class TestEvalMetric(unittest.TestCase):
                 "ndcg_1_ideal_dcg": expected_ideal_dcg,
                 "ndcg_1_dcg": expected_dcg,
                 "ndcg_1_value": expected_ndcg,
+            },
+        )
+
+        metric = NormalizedDiscountedCumulativeGain(at=3)
+        evaluation = metric.evaluate_query(
+            query_results=VespaResult(self.query_results2),
+            relevant_docs=self.labelled_data2[0]["relevant_docs"],
+            id_field="vespa_id_field",
+            default_score=0,
+        )
+        expected_dcg = 1 / math.log2(2) + 0 / math.log2(3) + 2 / math.log2(4)
+        expected_ideal_dcg = 2 / math.log2(2) + 1 / math.log2(3) + 0 / math.log2(4)
+        expected_ndcg = expected_dcg / expected_ideal_dcg
+        self.assertDictEqual(
+            evaluation,
+            {
+                "ndcg_3_ideal_dcg": expected_ideal_dcg,
+                "ndcg_3_dcg": expected_dcg,
+                "ndcg_3_value": expected_ndcg,
             },
         )
