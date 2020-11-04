c56c039e14ccfe782315c9391f207c13e9bd6280
tmartins <thigm85@gmail.com>
2020-11-04 11:21:58 +0100

    add data operations test for cloud

--- vespa/test_integration_vespa_cloud.py
+++ vespa/test_integration_vespa_cloud.py
@@ -60,7 +60,14 @@ class TestCloudDeployment(unittest.TestCase):
         )
 
     def test_data_operation(self):
-        self.assertIsInstance(self.app, Vespa)
+        #
+        # Make sure we start with no data with id = 1
+        #
+        _ = self.app.delete_data(schema="msmarco", data_id="1")
+        #
+        # Get data that does not exist
+        #
+        self.assertEqual(self.app.get_data(schema="msmarco", data_id="1").status_code, 404)
         #
         # Feed a data point
         #
@@ -74,6 +81,56 @@ class TestCloudDeployment(unittest.TestCase):
             },
         )
         self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+        #
+        # Get data that exist
+        #
+        response = self.app.get_data(schema="msmarco", data_id="1")
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(
+            response.json(),
+            {
+                "fields": {
+                    "id": "1",
+                    "title": "this is my first title",
+                    "body": "this is my first body",
+                },
+                "id": "id:msmarco:msmarco::1",
+                "pathId": "/document/v1/msmarco/msmarco/docid/1",
+            },
+        )
+        #
+        # Update data
+        #
+        response = self.app.update_data(
+            schema="msmarco", data_id="1", fields={"title": "this is my updated title"}
+        )
+        self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+        #
+        # Get the updated data point
+        #
+        response = self.app.get_data(schema="msmarco", data_id="1")
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(
+            response.json(),
+            {
+                "fields": {
+                    "id": "1",
+                    "title": "this is my updated title",
+                    "body": "this is my first body",
+                },
+                "id": "id:msmarco:msmarco::1",
+                "pathId": "/document/v1/msmarco/msmarco/docid/1",
+            },
+        )
+        #
+        # Delete a data point
+        #
+        response = self.app.delete_data(schema="msmarco", data_id="1")
+        self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+        #
+        # Deleted data should be gone
+        #
+        self.assertEqual(self.app.get_data(schema="msmarco", data_id="1").status_code, 404)
 
     def tearDown(self) -> None:
         shutil.rmtree(self.disk_folder, ignore_errors=True)
