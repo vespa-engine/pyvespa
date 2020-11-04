bb34ad619dee87163feb52fe5a95f4315b9fd2b7
tmartins <thigm85@gmail.com>
2020-11-04 11:15:12 +0100

    add test data operations

--- vespa/test_integration_docker.py
+++ vespa/test_integration_docker.py
@@ -42,17 +42,90 @@ class TestDockerDeployment(unittest.TestCase):
         self.disk_folder = os.path.join(os.getenv("WORK_DIR"), "sample_application")
 
     def test_deploy(self):
-        self.vespa_docker = VespaDocker()
+        self.vespa_docker = VespaDocker(port=8089)
         app = self.vespa_docker.deploy(
             application_package=self.app_package, disk_folder=self.disk_folder
         )
-
         self.assertTrue(
             any(re.match("Generation: [0-9]+", line) for line in app.deployment_message)
         )
+        self.assertEqual(app.get_application_status().status_code, 200)
+
+    def test_data_operation(self):
+        self.vespa_docker = VespaDocker(port=8089)
+        app = self.vespa_docker.deploy(
+            application_package=self.app_package, disk_folder=self.disk_folder
+        )
+        #
+        # Get data that does not exist
+        #
+        self.assertEqual(app.get_data(schema="msmarco", data_id="1").status_code, 404)
+        #
+        # Feed a data point
+        #
+        response = app.feed_data_point(
+            schema="msmarco",
+            data_id="1",
+            fields={
+                "id": "1",
+                "title": "this is my first title",
+                "body": "this is my first body",
+            },
+        )
+        self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+        #
+        # Get data that exist
+        #
+        response = app.get_data(schema="msmarco", data_id="1")
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(
+            response.json(),
+            {
+                "fields": {
+                    "id": "1",
+                    "title": "this is my first title",
+                    "body": "this is my first body",
+                },
+                "id": "id:msmarco:msmarco::1",
+                "pathId": "/document/v1/msmarco/msmarco/docid/1",
+            },
+        )
+        #
+        # Update data
+        #
+        response = app.update_data(
+            schema="msmarco", data_id="1", fields={"title": "this is my updated title"}
+        )
+        self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+        #
+        # Get the updated data point
+        #
+        response = app.get_data(schema="msmarco", data_id="1")
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(
+            response.json(),
+            {
+                "fields": {
+                    "id": "1",
+                    "title": "this is my updated title",
+                    "body": "this is my first body",
+                },
+                "id": "id:msmarco:msmarco::1",
+                "pathId": "/document/v1/msmarco/msmarco/docid/1",
+            },
+        )
+        #
+        # Delete a data point
+        #
+        response = app.delete_data(schema="msmarco", data_id="1")
+        self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+        #
+        # Deleted data should be gone
+        #
+        self.assertEqual(app.get_data(schema="msmarco", data_id="1").status_code, 404)
 
     def test_deploy_from_disk(self):
-        self.vespa_docker = VespaDocker()
+        self.vespa_docker = VespaDocker(port=8089)
         self.vespa_docker.export_application_package(
             dir_path=self.disk_folder, application_package=self.app_package
         )
@@ -70,7 +143,6 @@ class TestDockerDeployment(unittest.TestCase):
             application_package=self.app_package, disk_folder=self.disk_folder
         )
         print(app.get_data(schema="test", data_id="1"))
-
 
     def tearDown(self) -> None:
         shutil.rmtree(self.disk_folder, ignore_errors=True)
