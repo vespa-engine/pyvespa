8bd9f3233965afe711b51df01327a07658c38506
tmartins <thigm85@gmail.com>
2020-12-28 09:03:35 -0300

    fix doctest output

--- vespa/application.py
+++ vespa/application.py
@@ -43,7 +43,10 @@ class Vespa(object):
         :param output_file: Output file to write output messages.
 
         >>> Vespa(url = "https://cord19.vespa.ai")  # doctest: +SKIP
+
         >>> Vespa(url = "http://localhost", port = 8080)
+        Vespa(http://localhost, 8080)
+        
         >>> Vespa(url = "https://api.vespa-external.aws.oath.cloud", port = 4443, cert = "/path/to/cert-and-key.pem")  # doctest: +SKIP
 
         """
