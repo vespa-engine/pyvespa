7988b65b4e90fbe8b06c4e0b7d32a72cbd105d4f
tmartins <thigm85@gmail.com>
2021-01-20 06:37:36 -0300

    create docker integration test with OnnxModel

--- vespa/test_integration_docker.py
+++ vespa/test_integration_docker.py
@@ -2,10 +2,19 @@ import unittest
 import os
 import re
 import shutil
-from vespa.package import Document, Field
-from vespa.package import Schema, FieldSet, RankProfile
-from vespa.package import ApplicationPackage
-from vespa.package import VespaDocker
+from vespa.package import (
+    Document,
+    Field,
+    Schema,
+    QueryTypeField,
+    FieldSet,
+    OnnxModel,
+    Function,
+    SecondPhaseRanking,
+    RankProfile,
+    ApplicationPackage,
+    VespaDocker,
+)
 
 
 class TestDockerDeployment(unittest.TestCase):
@@ -136,6 +145,216 @@ class TestDockerDeployment(unittest.TestCase):
         self.assertTrue(
             any(re.match("Generation: [0-9]+", line) for line in app.deployment_message)
         )
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.disk_folder, ignore_errors=True)
+        self.vespa_docker.container.stop()
+        self.vespa_docker.container.remove()
+
+
+class TestOnnxModelDockerDeployment(unittest.TestCase):
+    def setUp(self) -> None:
+        #
+        # Create application package
+        #
+        self.app_package = ApplicationPackage(name="cord19")
+        self.app_package.schema.add_fields(
+            Field(name="cord_uid", type="string", indexing=["attribute", "summary"]),
+            Field(
+                name="title",
+                type="string",
+                indexing=["index", "summary"],
+                index="enable-bm25",
+            ),
+            Field(
+                name="doc_token_ids",
+                type="tensor<float>(d0[96])",
+                indexing=["attribute", "summary"],
+            ),
+        )
+        self.app_package.schema.add_field_set(
+            FieldSet(name="default", fields=["title"])
+        )
+        self.app_package.query_profile_type.add_fields(
+            QueryTypeField(
+                name="ranking.features.query(query_token_ids)",
+                type="tensor<float>(d0[32])",
+            )
+        )
+        self.app_package.schema.add_model(
+            OnnxModel(
+                model_name="bert_tiny",
+                model_file_path="resources/bert_tiny.onnx",
+                inputs={
+                    "input_ids": "input_ids",
+                    "token_type_ids": "token_type_ids",
+                    "attention_mask": "attention_mask",
+                },
+                outputs={"logits": "logits"},
+            )
+        )
+        self.app_package.schema.add_rank_profile(
+            RankProfile(
+                name="bert",
+                inherits="default",
+                constants={"TOKEN_NONE": 0, "TOKEN_CLS": 101, "TOKEN_SEP": 102},
+                functions=[
+                    Function(
+                        name="question_length",
+                        expression="sum(map(query(query_token_ids), f(a)(a > 0)))",
+                    ),
+                    Function(
+                        name="doc_length",
+                        expression="sum(map(attribute(doc_token_ids), f(a)(a > 0)))",
+                    ),
+                    Function(
+                        name="input_ids",
+                        expression="tensor<float>(d0[1],d1[128])(\n"
+                                   "    if (d1 == 0,\n"
+                                   "        TOKEN_CLS,\n"
+                                   "    if (d1 < question_length + 1,\n"
+                                   "        query(query_token_ids){d0:(d1-1)},\n"
+                                   "    if (d1 == question_length + 1,\n"
+                                   "        TOKEN_SEP,\n"
+                                   "    if (d1 < question_length + doc_length + 2,\n"
+                                   "        attribute(doc_token_ids){d0:(d1-question_length-2)},\n"
+                                   "    if (d1 == question_length + doc_length + 2,\n"
+                                   "        TOKEN_SEP,\n"
+                                   "        TOKEN_NONE\n"
+                                   "    ))))))",
+                    ),
+                    Function(
+                        name="attention_mask",
+                        expression="map(input_ids, f(a)(a > 0)) ",
+                    ),
+                    Function(
+                        name="token_type_ids",
+                        expression="tensor<float>(d0[1],d1[128])(\n"
+                                   "    if (d1 < question_length,\n"
+                                   "        0,\n"
+                                   "    if (d1 < question_length + doc_length,\n"
+                                   "        1,\n"
+                                   "        TOKEN_NONE\n"
+                                   "    )))",
+                    ),
+                    Function(
+                        name="eval",
+                        expression="tensor(x{}):{x1:onnxModel(bert_tiny).logits{d0:0,d1:0}}",
+                    ),
+
+                ],
+                first_phase="bm25(title)",
+                second_phase=SecondPhaseRanking(
+                    rerank_count=10, expression="sum(eval)"
+                ),
+                summary_features=[
+                    "onnxModel(bert_tiny).logits",
+                    "input_ids",
+                    "attention_mask",
+                    "token_type_ids",
+                ],
+            )
+
+        )
+        self.disk_folder = os.path.join(os.getenv("WORK_DIR"), "sample_application")
+
+    def test_deploy(self):
+        self.vespa_docker = VespaDocker(port=8089)
+        app = self.vespa_docker.deploy(
+            application_package=self.app_package, disk_folder=self.disk_folder
+        )
+        self.assertTrue(
+            any(re.match("Generation: [0-9]+", line) for line in app.deployment_message)
+        )
+        self.assertEqual(app.get_application_status().status_code, 200)
+
+    def test_data_operation(self):
+        # self.vespa_docker = VespaDocker(port=8089)
+        # app = self.vespa_docker.deploy(
+        #     application_package=self.app_package, disk_folder=self.disk_folder
+        # )
+        # #
+        # # Get data that does not exist
+        # #
+        # self.assertEqual(app.get_data(schema="msmarco", data_id="1").status_code, 404)
+        # #
+        # # Feed a data point
+        # #
+        # response = app.feed_data_point(
+        #     schema="msmarco",
+        #     data_id="1",
+        #     fields={
+        #         "id": "1",
+        #         "title": "this is my first title",
+        #         "body": "this is my first body",
+        #     },
+        # )
+        # self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+        # #
+        # # Get data that exist
+        # #
+        # response = app.get_data(schema="msmarco", data_id="1")
+        # self.assertEqual(response.status_code, 200)
+        # self.assertDictEqual(
+        #     response.json(),
+        #     {
+        #         "fields": {
+        #             "id": "1",
+        #             "title": "this is my first title",
+        #             "body": "this is my first body",
+        #         },
+        #         "id": "id:msmarco:msmarco::1",
+        #         "pathId": "/document/v1/msmarco/msmarco/docid/1",
+        #     },
+        # )
+        # #
+        # # Update data
+        # #
+        # response = app.update_data(
+        #     schema="msmarco", data_id="1", fields={"title": "this is my updated title"}
+        # )
+        # self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+        # #
+        # # Get the updated data point
+        # #
+        # response = app.get_data(schema="msmarco", data_id="1")
+        # self.assertEqual(response.status_code, 200)
+        # self.assertDictEqual(
+        #     response.json(),
+        #     {
+        #         "fields": {
+        #             "id": "1",
+        #             "title": "this is my updated title",
+        #             "body": "this is my first body",
+        #         },
+        #         "id": "id:msmarco:msmarco::1",
+        #         "pathId": "/document/v1/msmarco/msmarco/docid/1",
+        #     },
+        # )
+        # #
+        # # Delete a data point
+        # #
+        # response = app.delete_data(schema="msmarco", data_id="1")
+        # self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+        # #
+        # # Deleted data should be gone
+        # #
+        # self.assertEqual(app.get_data(schema="msmarco", data_id="1").status_code, 404)
+        pass
+
+    def test_deploy_from_disk(self):
+        # self.vespa_docker = VespaDocker(port=8089)
+        # self.vespa_docker.export_application_package(
+        #     dir_path=self.disk_folder, application_package=self.app_package
+        # )
+        # app = self.vespa_docker.deploy_from_disk(
+        #     application_name=self.app_package.name, disk_folder=self.disk_folder
+        # )
+        #
+        # self.assertTrue(
+        #     any(re.match("Generation: [0-9]+", line) for line in app.deployment_message)
+        # )
+        pass
 
     def tearDown(self) -> None:
         shutil.rmtree(self.disk_folder, ignore_errors=True)
