f9d1cf26034bb709e054216c06d5fb9e350a8621
tmartins <thigm85@gmail.com>
2021-01-21 16:20:39 -0300

    integration test for add_bert_ranking

--- vespa/test_integration_docker.py
+++ vespa/test_integration_docker.py
@@ -2,6 +2,10 @@ import unittest
 import os
 import re
 import shutil
+
+from transformers import BertForSequenceClassification, BertTokenizerFast
+
+from vespa.ml import BertModelConfig
 from vespa.package import (
     Document,
     Field,
@@ -255,6 +259,148 @@ class TestOnnxModelDockerDeployment(unittest.TestCase):
                 ],
             )
 
+        )
+        self.disk_folder = os.path.join(os.getenv("WORK_DIR"), "sample_application")
+
+    def test_deploy(self):
+        self.vespa_docker = VespaDocker(port=8089)
+        app = self.vespa_docker.deploy(
+            application_package=self.app_package, disk_folder=self.disk_folder
+        )
+        self.assertTrue(
+            any(re.match("Generation: [0-9]+", line) for line in app.deployment_message)
+        )
+        self.assertEqual(app.get_application_status().status_code, 200)
+
+    # def test_data_operation(self):
+    #     self.vespa_docker = VespaDocker(port=8089)
+    #     app = self.vespa_docker.deploy(
+    #         application_package=self.app_package, disk_folder=self.disk_folder
+    #     )
+    #     #
+    #     # Get data that does not exist
+    #     #
+    #     self.assertEqual(app.get_data(schema="msmarco", data_id="1").status_code, 404)
+    #     #
+    #     # Feed a data point
+    #     #
+    #     response = app.feed_data_point(
+    #         schema="msmarco",
+    #         data_id="1",
+    #         fields={
+    #             "id": "1",
+    #             "title": "this is my first title",
+    #             "body": "this is my first body",
+    #         },
+    #     )
+    #     self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+    #     #
+    #     # Get data that exist
+    #     #
+    #     response = app.get_data(schema="msmarco", data_id="1")
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertDictEqual(
+    #         response.json(),
+    #         {
+    #             "fields": {
+    #                 "id": "1",
+    #                 "title": "this is my first title",
+    #                 "body": "this is my first body",
+    #             },
+    #             "id": "id:msmarco:msmarco::1",
+    #             "pathId": "/document/v1/msmarco/msmarco/docid/1",
+    #         },
+    #     )
+    #     #
+    #     # Update data
+    #     #
+    #     response = app.update_data(
+    #         schema="msmarco", data_id="1", fields={"title": "this is my updated title"}
+    #     )
+    #     self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+    #     #
+    #     # Get the updated data point
+    #     #
+    #     response = app.get_data(schema="msmarco", data_id="1")
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertDictEqual(
+    #         response.json(),
+    #         {
+    #             "fields": {
+    #                 "id": "1",
+    #                 "title": "this is my updated title",
+    #                 "body": "this is my first body",
+    #             },
+    #             "id": "id:msmarco:msmarco::1",
+    #             "pathId": "/document/v1/msmarco/msmarco/docid/1",
+    #         },
+    #     )
+    #     #
+    #     # Delete a data point
+    #     #
+    #     response = app.delete_data(schema="msmarco", data_id="1")
+    #     self.assertEqual(response.json()["id"], "id:msmarco:msmarco::1")
+    #     #
+    #     # Deleted data should be gone
+    #     #
+    #     self.assertEqual(app.get_data(schema="msmarco", data_id="1").status_code, 404)
+
+
+    # def test_deploy_from_disk(self):
+    #     self.vespa_docker = VespaDocker(port=8089)
+    #     self.vespa_docker.export_application_package(
+    #         dir_path=self.disk_folder, application_package=self.app_package
+    #     )
+    #     app = self.vespa_docker.deploy_from_disk(
+    #         application_name=self.app_package.name, disk_folder=self.disk_folder
+    #     )
+    #
+    #     self.assertTrue(
+    #         any(re.match("Generation: [0-9]+", line) for line in app.deployment_message)
+    #     )
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.disk_folder, ignore_errors=True)
+        self.vespa_docker.container.stop()
+        self.vespa_docker.container.remove()
+
+
+class TestBertModelDockerDeployment(unittest.TestCase):
+    def setUp(self) -> None:
+        #
+        # Create application package
+        #
+        self.app_package = ApplicationPackage(name="cord19")
+        self.app_package.schema.add_fields(
+            Field(name="cord_uid", type="string", indexing=["attribute", "summary"]),
+            Field(name="title", type="string", indexing=["index", "summary"], index="enable-bm25")
+        )
+        self.app_package.schema.add_field_set(
+            FieldSet(name="default", fields=["title"])
+        )
+        self.app_package.schema.add_rank_profile(
+            RankProfile(name="bm25", first_phase="bm25(title)")
+        )
+
+        tokenizer = BertTokenizerFast.from_pretrained("prajjwal1/bert-tiny")
+        model = BertForSequenceClassification.from_pretrained(
+            "prajjwal1/bert-tiny")  # This could be any pytorch BERT model
+
+        bert_config = BertModelConfig(
+            model_id="pretrained_bert_tiny",
+            tokenizer=tokenizer,
+            query_input_size=32,
+            doc_input_size=96
+        )
+
+        self.app_package.add_bert_ranking(
+            model_config=bert_config,
+            model=model,
+            inherits="default",
+            first_phase="bm25(title)",
+            second_phase=SecondPhaseRanking(
+                rerank_count=10, expression="logit0"
+            ),
         )
         self.disk_folder = os.path.join(os.getenv("WORK_DIR"), "sample_application")
 
