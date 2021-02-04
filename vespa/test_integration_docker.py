f3fbb15df85a8778e64938f2911788878e7e2927
tmartins <thigm85@gmail.com>
2021-02-04 08:33:13 -0300

    use add_model_ranking and add test_data_operation with onnx models

--- vespa/test_integration_docker.py
+++ vespa/test_integration_docker.py
@@ -6,15 +6,13 @@ from vespa.package import (
     Document,
     Field,
     Schema,
-    QueryTypeField,
     FieldSet,
-    OnnxModel,
-    Function,
     SecondPhaseRanking,
     RankProfile,
     ApplicationPackage,
     VespaDocker,
 )
+from vespa.ml import BertModelConfig
 
 
 class TestDockerDeployment(unittest.TestCase):
@@ -166,91 +164,25 @@ class TestOnnxModelDockerDeployment(unittest.TestCase):
                 indexing=["index", "summary"],
                 index="enable-bm25",
             ),
-            Field(
-                name="doc_token_ids",
-                type="tensor<float>(d0[96])",
-                indexing=["attribute", "summary"],
-            ),
         )
         self.app_package.schema.add_field_set(
             FieldSet(name="default", fields=["title"])
         )
-        self.app_package.query_profile_type.add_fields(
-            QueryTypeField(
-                name="ranking.features.query(query_token_ids)",
-                type="tensor<float>(d0[32])",
-            )
-        )
-        self.app_package.schema.add_model(
-            OnnxModel(
-                model_name="bert_tiny",
-                model_file_path=os.path.join(
-                    os.getenv("RESOURCES_DIR"), "bert_tiny.onnx"
-                ),
-                inputs={
-                    "input_ids": "input_ids",
-                    "token_type_ids": "token_type_ids",
-                    "attention_mask": "attention_mask",
-                },
-                outputs={"logits": "logits"},
-            )
-        )
         self.app_package.schema.add_rank_profile(
-            RankProfile(
-                name="bert",
-                inherits="default",
-                constants={"TOKEN_NONE": 0, "TOKEN_CLS": 101, "TOKEN_SEP": 102},
-                functions=[
-                    Function(
-                        name="question_length",
-                        expression="sum(map(query(query_token_ids), f(a)(a > 0)))",
-                    ),
-                    Function(
-                        name="doc_length",
-                        expression="sum(map(attribute(doc_token_ids), f(a)(a > 0)))",
-                    ),
-                    Function(
-                        name="input_ids",
-                        expression="tensor<float>(d0[1],d1[128])(\n"
-                        "    if (d1 == 0,\n"
-                        "        TOKEN_CLS,\n"
-                        "    if (d1 < question_length + 1,\n"
-                        "        query(query_token_ids){d0:(d1-1)},\n"
-                        "    if (d1 == question_length + 1,\n"
-                        "        TOKEN_SEP,\n"
-                        "    if (d1 < question_length + doc_length + 2,\n"
-                        "        attribute(doc_token_ids){d0:(d1-question_length-2)},\n"
-                        "    if (d1 == question_length + doc_length + 2,\n"
-                        "        TOKEN_SEP,\n"
-                        "        TOKEN_NONE\n"
-                        "    ))))))",
-                    ),
-                    Function(
-                        name="attention_mask",
-                        expression="map(input_ids, f(a)(a > 0)) ",
-                    ),
-                    Function(
-                        name="token_type_ids",
-                        expression="tensor<float>(d0[1],d1[128])(\n"
-                        "    if (d1 < question_length,\n"
-                        "        0,\n"
-                        "    if (d1 < question_length + doc_length,\n"
-                        "        1,\n"
-                        "        TOKEN_NONE\n"
-                        "    )))",
-                    ),
-                ],
-                first_phase="bm25(title)",
-                second_phase=SecondPhaseRanking(
-                    rerank_count=10, expression="sum(onnx(bert_tiny).logits{d0:0,d1:0})"
-                ),
-                summary_features=[
-                    "onnx(bert_tiny).logits",
-                    "input_ids",
-                    "attention_mask",
-                    "token_type_ids",
-                ],
-            )
+            RankProfile(name="bm25", first_phase="bm25(title)")
+        )
+        self.bert_config = BertModelConfig(
+            model_id="pretrained_bert_tiny",
+            tokenizer="google/bert_uncased_L-2_H-128_A-2",
+            model="google/bert_uncased_L-2_H-128_A-2",
+            query_input_size=32,
+            doc_input_size=96,
+        )
+        self.app_package.add_model_ranking(
+            model_config=self.bert_config,
+            inherits="default",
+            first_phase="bm25(title)",
+            second_phase=SecondPhaseRanking(rerank_count=10, expression="logit1"),
         )
         self.disk_folder = os.path.join(os.getenv("WORK_DIR"), "sample_application")
 
@@ -263,6 +195,98 @@ class TestOnnxModelDockerDeployment(unittest.TestCase):
             any(re.match("Generation: [0-9]+", line) for line in app.deployment_message)
         )
         self.assertEqual(app.get_application_status().status_code, 200)
+
+    def test_data_operation(self):
+        self.vespa_docker = VespaDocker(port=8089)
+        app = self.vespa_docker.deploy(
+            application_package=self.app_package, disk_folder=self.disk_folder
+        )
+        #
+        # Get data that does not exist
+        #
+        self.assertEqual(app.get_data(schema="cord19", data_id="1").status_code, 404)
+        #
+        # Feed a data point
+        #
+        fields = {
+            "cord_uid": "1",
+            "title": "this is my first title",
+        }
+        fields.update(self.bert_config.doc_fields(text=str(fields["title"])))
+        response = app.feed_data_point(
+            schema="cord19",
+            data_id="1",
+            fields=fields,
+        )
+        self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
+        #
+        # Get data that exist
+        #
+        response = app.get_data(schema="cord19", data_id="1")
+        self.assertEqual(response.status_code, 200)
+        embedding_values = fields["pretrained_bert_tiny_doc_token_ids"]["values"]
+        self.assertDictEqual(
+            response.json(),
+            {
+                "fields": {
+                    "cord_uid": "1",
+                    "title": "this is my first title",
+                    "pretrained_bert_tiny_doc_token_ids": {
+                        "cells": [
+                            {
+                                "address": {"d0": str(x)},
+                                "value": float(embedding_values[x]),
+                            }
+                            for x in range(len(embedding_values))
+                        ]
+                    },
+                },
+                "id": "id:cord19:cord19::1",
+                "pathId": "/document/v1/cord19/cord19/docid/1",
+            },
+        )
+        #
+        # Update data
+        #
+        fields = {"title": "this is my updated title"}
+        fields.update(self.bert_config.doc_fields(text=str(fields["title"])))
+        response = app.update_data(schema="cord19", data_id="1", fields=fields)
+        self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
+        #
+        # Get the updated data point
+        #
+        response = app.get_data(schema="cord19", data_id="1")
+        self.assertEqual(response.status_code, 200)
+        embedding_values = fields["pretrained_bert_tiny_doc_token_ids"]["values"]
+        self.assertDictEqual(
+            response.json(),
+            {
+                "fields": {
+                    "cord_uid": "1",
+                    "title": "this is my updated title",
+                    "pretrained_bert_tiny_doc_token_ids": {
+                        "cells": [
+                            {
+                                "address": {"d0": str(x)},
+                                "value": float(embedding_values[x]),
+                            }
+                            for x in range(len(embedding_values))
+                        ]
+                    },
+                },
+                "id": "id:cord19:cord19::1",
+                "pathId": "/document/v1/cord19/cord19/docid/1",
+            },
+        )
+        #
+        # Delete a data point
+        #
+        response = app.delete_data(schema="cord19", data_id="1")
+        self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
+        #
+        # Deleted data should be gone
+        #
+        self.assertEqual(app.get_data(schema="cord19", data_id="1").status_code, 404)
 
     def tearDown(self) -> None:
         shutil.rmtree(self.disk_folder, ignore_errors=True)
