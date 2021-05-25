1918116b98099063c683618a5c487aaf87dcc624
tmartins <thigm85@gmail.com>
2021-05-25 09:25:12 -0300

    test input output

--- vespa/test_integration_vespa_cloud.py
+++ vespa/test_integration_vespa_cloud.py
@@ -234,102 +234,13 @@ class TestOnnxModelCloudDeployment(unittest.TestCase):
             instance=self.instance_name, disk_folder=self.disk_folder
         )
 
-    def test_data_operation(self):
-        #
-        # Get data that does not exist
-        #
-        self.assertEqual(
-            self.app.get_data(schema="cord19", data_id="1").status_code, 404
-        )
-        #
-        # Feed a data point
-        #
-        fields = {
-            "cord_uid": "1",
-            "title": "this is my first title",
-        }
-        fields.update(self.bert_config.doc_fields(text=str(fields["title"])))
-        response = self.app.feed_data_point(
-            schema="cord19",
-            data_id="1",
-            fields=fields,
-        )
-        self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
-        #
-        # Get data that exist
-        #
-        response = self.app.get_data(schema="cord19", data_id="1")
-        self.assertEqual(response.status_code, 200)
-        embedding_values = fields["pretrained_bert_tiny_doc_token_ids"]["values"]
-        self.assertDictEqual(
-            response.json(),
-            {
-                "fields": {
-                    "cord_uid": "1",
-                    "title": "this is my first title",
-                    "pretrained_bert_tiny_doc_token_ids": {
-                        "cells": [
-                            {
-                                "address": {"d0": str(x)},
-                                "value": float(embedding_values[x]),
-                            }
-                            for x in range(len(embedding_values))
-                        ]
-                    },
-                },
-                "id": "id:cord19:cord19::1",
-                "pathId": "/document/v1/cord19/cord19/docid/1",
-            },
-        )
-        #
-        # Update data
-        #
-        fields = {"title": "this is my updated title"}
-        fields.update(self.bert_config.doc_fields(text=str(fields["title"])))
-        response = self.app.update_data(schema="cord19", data_id="1", fields=fields)
-        self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
-        #
-        # Get the updated data point
-        #
-        response = self.app.get_data(schema="cord19", data_id="1")
-        self.assertEqual(response.status_code, 200)
-        embedding_values = fields["pretrained_bert_tiny_doc_token_ids"]["values"]
-        self.assertDictEqual(
-            response.json(),
-            {
-                "fields": {
-                    "cord_uid": "1",
-                    "title": "this is my updated title",
-                    "pretrained_bert_tiny_doc_token_ids": {
-                        "cells": [
-                            {
-                                "address": {"d0": str(x)},
-                                "value": float(embedding_values[x]),
-                            }
-                            for x in range(len(embedding_values))
-                        ]
-                    },
-                },
-                "id": "id:cord19:cord19::1",
-                "pathId": "/document/v1/cord19/cord19/docid/1",
-            },
-        )
-        #
-        # Delete a data point
-        #
-        response = self.app.delete_data(schema="cord19", data_id="1")
-        self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
-        #
-        # Deleted data should be gone
-        #
-        self.assertEqual(
-            self.app.get_data(schema="cord19", data_id="1").status_code, 404
-        )
-
-    def _parse_vespa_tensor(self, hit, feature):
-        return [x["value"] for x in hit["fields"]["summaryfeatures"][feature]["cells"]]
-
-    # def test_rank_input_output(self):
+    # def test_data_operation(self):
+    #     #
+    #     # Get data that does not exist
+    #     #
+    #     self.assertEqual(
+    #         self.app.get_data(schema="cord19", data_id="1").status_code, 404
+    #     )
     #     #
     #     # Feed a data point
     #     #
@@ -345,51 +256,140 @@ class TestOnnxModelCloudDeployment(unittest.TestCase):
     #     )
     #     self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
     #     #
-    #     # Run a test query
+    #     # Get data that exist
     #     #
-    #     result = self.app.query(
-    #         query="this is a test",
-    #         query_model=QueryModel(
-    #             query_properties=[
-    #                 QueryRankingFeature(
-    #                     name=self.bert_config.query_token_ids_name,
-    #                     mapping=self.bert_config.query_tensor_mapping,
-    #                 )
-    #             ],
-    #             match_phase=OR(),
-    #             rank_profile=Ranking(name="pretrained_bert_tiny"),
-    #         ),
+    #     response = self.app.get_data(schema="cord19", data_id="1")
+    #     self.assertEqual(response.status_code, 200)
+    #     embedding_values = fields["pretrained_bert_tiny_doc_token_ids"]["values"]
+    #     self.assertDictEqual(
+    #         response.json(),
+    #         {
+    #             "fields": {
+    #                 "cord_uid": "1",
+    #                 "title": "this is my first title",
+    #                 "pretrained_bert_tiny_doc_token_ids": {
+    #                     "cells": [
+    #                         {
+    #                             "address": {"d0": str(x)},
+    #                             "value": float(embedding_values[x]),
+    #                         }
+    #                         for x in range(len(embedding_values))
+    #                     ]
+    #                 },
+    #             },
+    #             "id": "id:cord19:cord19::1",
+    #             "pathId": "/document/v1/cord19/cord19/docid/1",
+    #         },
     #     )
-    #     vespa_input_ids = self._parse_vespa_tensor(
-    #         result.hits[0], "rankingExpression(input_ids)"
+    #     #
+    #     # Update data
+    #     #
+    #     fields = {"title": "this is my updated title"}
+    #     fields.update(self.bert_config.doc_fields(text=str(fields["title"])))
+    #     response = self.app.update_data(schema="cord19", data_id="1", fields=fields)
+    #     self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
+    #     #
+    #     # Get the updated data point
+    #     #
+    #     response = self.app.get_data(schema="cord19", data_id="1")
+    #     self.assertEqual(response.status_code, 200)
+    #     embedding_values = fields["pretrained_bert_tiny_doc_token_ids"]["values"]
+    #     self.assertDictEqual(
+    #         response.json(),
+    #         {
+    #             "fields": {
+    #                 "cord_uid": "1",
+    #                 "title": "this is my updated title",
+    #                 "pretrained_bert_tiny_doc_token_ids": {
+    #                     "cells": [
+    #                         {
+    #                             "address": {"d0": str(x)},
+    #                             "value": float(embedding_values[x]),
+    #                         }
+    #                         for x in range(len(embedding_values))
+    #                     ]
+    #                 },
+    #             },
+    #             "id": "id:cord19:cord19::1",
+    #             "pathId": "/document/v1/cord19/cord19/docid/1",
+    #         },
     #     )
-    #     vespa_attention_mask = self._parse_vespa_tensor(
-    #         result.hits[0], "rankingExpression(attention_mask)"
+    #     #
+    #     # Delete a data point
+    #     #
+    #     response = self.app.delete_data(schema="cord19", data_id="1")
+    #     self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
+    #     #
+    #     # Deleted data should be gone
+    #     #
+    #     self.assertEqual(
+    #         self.app.get_data(schema="cord19", data_id="1").status_code, 404
     #     )
-    #     vespa_token_type_ids = self._parse_vespa_tensor(
-    #         result.hits[0], "rankingExpression(token_type_ids)"
-    #     )
-    #
-    #     expected_inputs = self.bert_config.create_encodings(
-    #         queries=["this is a test"], docs=["this is my first title"]
-    #     )
-    #     self.assertEqual(vespa_input_ids, expected_inputs["input_ids"][0])
-    #     self.assertEqual(vespa_attention_mask, expected_inputs["attention_mask"][0])
-    #     self.assertEqual(vespa_token_type_ids, expected_inputs["token_type_ids"][0])
-    #
-    #     expected_logits = self.bert_config.predict(
-    #         queries=["this is a test"], docs=["this is my first title"]
-    #     )
-    #     self.assertAlmostEqual(
-    #         result.hits[0]["fields"]["summaryfeatures"]["rankingExpression(logit0)"],
-    #         expected_logits[0][0],
-    #         5,
-    #     )
-    #     self.assertAlmostEqual(
-    #         result.hits[0]["fields"]["summaryfeatures"]["rankingExpression(logit1)"],
-    #         expected_logits[0][1],
-    #         5,
-    #     )
+
+    def _parse_vespa_tensor(self, hit, feature):
+        return [x["value"] for x in hit["fields"]["summaryfeatures"][feature]["cells"]]
+
+    def test_rank_input_output(self):
+        #
+        # Feed a data point
+        #
+        fields = {
+            "cord_uid": "1",
+            "title": "this is my first title",
+        }
+        fields.update(self.bert_config.doc_fields(text=str(fields["title"])))
+        response = self.app.feed_data_point(
+            schema="cord19",
+            data_id="1",
+            fields=fields,
+        )
+        self.assertEqual(response.json()["id"], "id:cord19:cord19::1")
+        #
+        # Run a test query
+        #
+        result = self.app.query(
+            query="this is a test",
+            query_model=QueryModel(
+                query_properties=[
+                    QueryRankingFeature(
+                        name=self.bert_config.query_token_ids_name,
+                        mapping=self.bert_config.query_tensor_mapping,
+                    )
+                ],
+                match_phase=OR(),
+                rank_profile=Ranking(name="pretrained_bert_tiny"),
+            ),
+        )
+        vespa_input_ids = self._parse_vespa_tensor(
+            result.hits[0], "rankingExpression(input_ids)"
+        )
+        vespa_attention_mask = self._parse_vespa_tensor(
+            result.hits[0], "rankingExpression(attention_mask)"
+        )
+        vespa_token_type_ids = self._parse_vespa_tensor(
+            result.hits[0], "rankingExpression(token_type_ids)"
+        )
+
+        expected_inputs = self.bert_config.create_encodings(
+            queries=["this is a test"], docs=["this is my first title"]
+        )
+        self.assertEqual(vespa_input_ids, expected_inputs["input_ids"][0])
+        self.assertEqual(vespa_attention_mask, expected_inputs["attention_mask"][0])
+        self.assertEqual(vespa_token_type_ids, expected_inputs["token_type_ids"][0])
+
+        expected_logits = self.bert_config.predict(
+            queries=["this is a test"], docs=["this is my first title"]
+        )
+        self.assertAlmostEqual(
+            result.hits[0]["fields"]["summaryfeatures"]["rankingExpression(logit0)"],
+            expected_logits[0][0],
+            5,
+        )
+        self.assertAlmostEqual(
+            result.hits[0]["fields"]["summaryfeatures"]["rankingExpression(logit1)"],
+            expected_logits[0][1],
+            5,
+        )
 
     def tearDown(self) -> None:
         shutil.rmtree(self.disk_folder, ignore_errors=True)
