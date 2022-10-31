7c3da1855d7db5d71fb957db5258dd9b1cb741c8
tmartins <thigm85@gmail.com>
2022-10-31 15:15:32 -0300

    remove unnecessary code, replaced by learntorank.query.Tokenize

--- vespa/experimental/ranking.py
+++ vespa/experimental/ranking.py
@@ -16,59 +16,6 @@ from vespa.package import (
     QueryField,
 )
 
-REPLACE_SYMBOLS = ["(", ")", " -", " +"]
-QUOTES = [
-    "\u0022",  # quotation mark (")
-    "\u0027",  # apostrophe (')
-    "\u00ab",  # left-pointing double-angle quotation mark
-    "\u00bb",  # right-pointing double-angle quotation mark
-    "\u2018",  # left single quotation mark
-    "\u2019",  # right single quotation mark
-    "\u201a",  # single low-9 quotation mark
-    "\u201b",  # single high-reversed-9 quotation mark
-    "\u201c",  # left double quotation mark
-    "\u201d",  # right double quotation mark
-    "\u201e",  # double low-9 quotation mark
-    "\u201f",  # double high-reversed-9 quotation mark
-    "\u2039",  # single left-pointing angle quotation mark
-    "\u203a",  # single right-pointing angle quotation mark
-    "\u300c",  # left corner bracket
-    "\u300d",  # right corner bracket
-    "\u300e",  # left white corner bracket
-    "\u300f",  # right white corner bracket
-    "\u301d",  # reversed double prime quotation mark
-    "\u301e",  # double prime quotation mark
-    "\u301f",  # low double prime quotation mark
-    "\ufe41",  # presentation form for vertical left corner bracket
-    "\ufe42",  # presentation form for vertical right corner bracket
-    "\ufe43",  # presentation form for vertical left corner white bracket
-    "\ufe44",  # presentation form for vertical right corner white bracket
-    "\uff02",  # fullwidth quotation mark
-    "\uff07",  # fullwidth apostrophe
-    "\uff62",  # halfwidth left corner bracket
-    "\uff63",  # halfwidth right corner bracket
-]
-REPLACE_SYMBOLS.extend(QUOTES)
-
-
-class Dataset:
-    def __init__(self):
-        """
-        Convenient functions to remove special symbols from queries.
-        """
-        pass
-
-    @staticmethod
-    def replace_symbols(x):
-        for symbol in REPLACE_SYMBOLS:
-            x = x.replace(symbol, "")
-        return x
-
-    @staticmethod
-    def parse_query(query):
-        return re.sub(" +", " ", Dataset.replace_symbols(query)).strip()
-
-
 class BeirData:
     def __init__(self, data_dir: str):
         """
