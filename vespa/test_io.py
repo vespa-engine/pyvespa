3312a963358da5381d2792cbbaefa59e7e77b932
Jo Kristian Bergum <bergum@yahooinc.com>
2023-11-08 17:44:41 +0100

    Remove deprecated apis (#617)
    * Remove deprecated apis
    
    * Update notebooks
    
    * More notebook fixes
    
    * more notebook fixes
    
    * install vespa-cli+zstd and how we run notebooks
    
    * fix notebook, create isn't supported in the dict
    
    * Add more asserts for notebooks
    
    * relax assert
    
    * Make the assert right this time
    
    * Improve multi-vector notebook
    
    * Improve notebooks

--- vespa/test_io.py
+++ vespa/test_io.py
@@ -1,9 +1,6 @@
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 import unittest
-from pandas import DataFrame
-from pandas.testing import assert_frame_equal
-
 from vespa.io import VespaQueryResponse
 
 
@@ -129,84 +126,4 @@ class TestVespaResult(unittest.TestCase):
                     },
                 }
             ],
-        )
-
-    def test_get_hits_none(self):
-        empty_hits_vespa_result = VespaQueryResponse(
-            json=self.raw_vespa_result_empty_hits, status_code=None, url=None
-        )
-        self.assertEqual(
-            empty_hits_vespa_result.get_hits(format_function=None),
-            empty_hits_vespa_result.hits,
-        )
-        vespa_result = VespaQueryResponse(
-            json=self.raw_vespa_result, status_code=None, url=None
-        )
-        self.assertEqual(
-            vespa_result.get_hits(format_function=None),
-            vespa_result.hits,
-        )
-
-    def test_get_hits_trec_format(self):
-        empty_hits_vespa_result = VespaQueryResponse(
-            json=self.raw_vespa_result_empty_hits, status_code=None, url=None
-        )
-        self.assertTrue(empty_hits_vespa_result.get_hits().empty)
-        vespa_result = VespaQueryResponse(
-            json=self.raw_vespa_result, status_code=None, url=None
-        )
-        assert_frame_equal(
-            vespa_result.get_hits(),
-            DataFrame(
-                data={
-                    "qid": [0],
-                    "doc_id": ["id:covid-19:doc::40215"],
-                    "score": [30.368213170494712],
-                    "rank": [0],
-                },
-                columns=["qid", "doc_id", "score", "rank"],
-            ),
-        )
-        vespa_result = VespaQueryResponse(
-            json=self.raw_vespa_result_multiple, status_code=None, url=None
-        )
-        assert_frame_equal(
-            vespa_result.get_hits(),
-            DataFrame(
-                data={
-                    "qid": [0, 0, 0],
-                    "doc_id": [
-                        "id:covid-19:doc::142863",
-                        "id:covid-19:doc::31328",
-                        "id:covid-19:doc::187156",
-                    ],
-                    "score": [
-                        11.893775281559614,
-                        11.891779491908013,
-                        11.887045666057702,
-                    ],
-                    "rank": [0, 1, 2],
-                },
-                columns=["qid", "doc_id", "score", "rank"],
-            ),
-        )
-        assert_frame_equal(
-            vespa_result.get_hits(id_field="cord_uid", qid=2),
-            DataFrame(
-                data={
-                    "qid": [2, 2, 2],
-                    "doc_id": [
-                        "0p6vrujx",
-                        "moy0u7n5",
-                        "rhmywn8n",
-                    ],
-                    "score": [
-                        11.893775281559614,
-                        11.891779491908013,
-                        11.887045666057702,
-                    ],
-                    "rank": [0, 1, 2],
-                },
-                columns=["qid", "doc_id", "score", "rank"],
-            ),
         )
