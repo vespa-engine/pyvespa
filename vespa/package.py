a982de2888ce012637e1fdfd8fb479201eb60159
Torstein Egge <tmaregge@gmail.com>
2024-02-21 14:13:47 +0100

    Add more checks to app name test + update error

--- vespa/package.py
+++ vespa/package.py
@@ -2076,9 +2076,9 @@ class ApplicationPackage(object):
         It will create a default :class:`Schema`, :class:`QueryProfile` and :class:`QueryProfileType` that you can then
         populate with specifics of your application.
         """
-        if not name.isalnum():
+        if not name[0].isalpha() or not name.islower() or not name.isalnum() or not len(name) <= 20:
             raise ValueError(
-                "Application package name can only contain [a-zA-Z0-9], was '{}'".format(
+                "Application name must start with a letter, must be lowercase, can only contain [a-z0-9] and may contain no more than 20 characters, was '{}'".format(
                     name
                 )
             )
