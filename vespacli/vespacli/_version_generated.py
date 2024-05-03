24c46500fbe2931d827ac54a8f773d5365aea41e
thomasht86 <thomas@vespa.ai>
2024-05-03 14:14:44 +0200

    new version

--- vespacli/vespacli/_version_generated.py
+++ vespacli/vespacli/_version_generated.py
@@ -1,1 +1,1 @@
-vespa_version = "8.338.38"  # Automatically updated by github actions bot vespacli/_version_generated.py
+vespa_version = "8.329.17"  # Automatically updated by github actions bot vespacli/_version_generated.py
