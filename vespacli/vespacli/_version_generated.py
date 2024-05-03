85d33dbe59798d8a716cb3d8ed110391f5013c77
thomasht86 <thomas@vespa.ai>
2024-05-03 10:33:47 +0200

    upd version - run test matrix

--- vespacli/vespacli/_version_generated.py
+++ vespacli/vespacli/_version_generated.py
@@ -1,1 +1,1 @@
-vespa_version = "8.331.34"  # Automatically updated by github actions bot vespacli/_version_generated.py
+vespa_version = "8.338.38"  # Automatically updated by github actions bot vespacli/_version_generated.py
