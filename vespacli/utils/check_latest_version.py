fa2fe63a65fb3e4024d5db6fc1936ff6c0a181ae
Marlon Saglia <marlon@vespa.ai>
2024-08-29 13:18:16 +0200

    feat: update PyPI release workflow to use OIDC

--- vespacli/utils/check_latest_version.py
+++ vespacli/utils/check_latest_version.py
@@ -1,13 +1,28 @@
 from download_binaries import VespaBinaryDownloader
-from vespacli._version_generated import vespa_version
 import sys
+import requests
+from packaging import version
+
+
+def get_latest_pypi_version() -> version.Version:
+    response = requests.get(
+        "https://pypi.org/simple/vespacli/",
+        headers={"Accept": "application/vnd.pypi.simple.v1+json"}
+    )
+    latest_version = response.json()["releases"][-1]["version"]
+    return version.parse(latest_version)
+
+
+def get_latest_github_version() -> version.Version:
+    downloader = VespaBinaryDownloader()
+    return version.parse(downloader.get_latest_version())
+
 
 if __name__ == "__main__":
-    downloader = VespaBinaryDownloader()
-    new_version = downloader.get_latest_version()
-    found_newer = new_version != vespa_version
-    if found_newer:
-        print(f"{new_version}")
+    gh_release = get_latest_github_version()
+    pypi_release = get_latest_pypi_version()
+    if gh_release > pypi_release:
+        print(f"{0}" % gh_release)
     else:
         print("NA")
     sys.exit(0)
