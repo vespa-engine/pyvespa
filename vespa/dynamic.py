41ffe792d4b63e6d356ea0b2ff06cfe6381b7c81
thomasht86 <thomas@vespa.ai>
2024-09-08 06:58:32 +0200

    looking like something

--- vespa/dynamic.py
+++ vespa/dynamic.py
@@ -1,42 +1,66 @@
-from functools import partial
+from fastcore.utils import tuplify
+import types
+from xml.sax.saxutils import escape
+from fastcore.utils import patch
 
 
-class XMLTag:
-    def __init__(self, tag, *children, **attrs):
-        self.tag = tag
-        self.attrs = attrs
-        self.children = list(children)
+# %% ../nbs/11_xml.ipynb
+class FT:
+    "A 'Fast Tag' structure, containing `tag`, `children`, and `attrs`"
 
-    def __call__(self, *children):
-        self.children.extend(children)
+    @staticmethod
+    def sanitize_tag_name(tag: str) -> str:
+        "Convert invalid tag names (with '-') to valid Python identifiers (with '_')"
+        return tag.replace("-", "_")
+
+    @staticmethod
+    def restore_tag_name(tag: str) -> str:
+        "Restore sanitized tag names back to the original names for XML generation"
+        return tag.replace("_", "-")
+
+    def __init__(self, tag: str, cs: tuple, attrs: dict = None, void_=False, **kwargs):
+        assert isinstance(cs, tuple)
+        self.tag = self.sanitize_tag_name(tag)  # Sanitize tag name
+        self.children, self.attrs = cs, attrs or {}
+        self.void_ = void_
+
+    def __setattr__(self, k, v):
+        if k.startswith("__") or k in ("tag", "children", "attrs", "void_"):
+            return super().__setattr__(k, v)
+        self.attrs[k.lstrip("_").replace("_", "-")] = v
+
+    def __getattr__(self, k):
+        if k.startswith("__"):
+            raise AttributeError(k)
+        return self.get(k)
+
+    @property
+    def list(self):
+        return [self.tag, self.children, self.attrs]
+
+    def get(self, k, default=None):
+        return self.attrs.get(k.lstrip("_").replace("_", "-"), default)
+
+    def __repr__(self):
+        return f"{self.tag}({self.children},{self.attrs})"
+
+    def __add__(self, b):
+        self.children = self.children + tuplify(b)
         return self
 
-    def to_xml(self, level=0, indent="  "):
-        spaces = indent * level
-        attrs = " ".join(f'{k}="{v}"' for k, v in self.attrs.items())
-        open_tag = f"{spaces}<{self.tag} {attrs}>" if attrs else f"{spaces}<{self.tag}>"
+    def __getitem__(self, idx):
+        return self.children[idx]
 
-        if not self.children:
-            return f"{open_tag[:-1]} />"
-
-        children_xml = "\n".join(
-            child.to_xml(level + 1, indent)
-            if isinstance(child, XMLTag)
-            else f"{spaces}{indent}{child}"
-            for child in self.children
-        )
-        return f"{open_tag}\n{children_xml}\n{spaces}</{self.tag}>"
+    def __iter__(self):
+        return iter(self.children)
 
 
-def VT(version="1.0", encoding="utf-8"):
-    return f'<?xml version="{version}" encoding="{encoding}" ?>'
-
-
-# Define the tags to be exported
-__all__ = list(
+# List of XML tags (customized for Vespa configuration)
+xml_tags = list(
     map(
-        str.capitalize,
+        FT.sanitize_tag_name,
         [
+            "services",
             "content",
             "documents",
             "document",
@@ -118,10 +142,6 @@ __all__ = list(
             "minimum",
             "min-wait-after-coverage-factor",
             "max-wait-after-coverage-factor",
-            "dispatch DEPRECATED",
-            "num-dispatch-groups DEPRECATED",
-            "group DEPRECATED",
-            "nodeDEPRECATED",
             "tuning",
             "bucket-splitting",
             "min-node-ratio-per-group",
@@ -155,27 +175,191 @@ __all__ = list(
             "slobrok",
             "adminserver",
             "container",
-            "documentapi",
-            "documentprocessing",
+            "document-api",
             "nodes",
             "jvm",
             "node",
             "search",
             "content",
-            "minredundancy",
-            "documents",
-            "document",
+            "min-redundancy",
             "component",
-            "transformermodel",
-            "tokenizermodel",
+            "transformer-model",
+            "tokenizer-model",
+            "prepend",
+            "query",
             "resources",
             "gpu",
-            "termscorethreshold",
         ],
     )
-) + ["VT"]
+)
+# %% auto 0
+__all__ = [
+    "FT",
+    "attrmap",
+    "valmap",
+    "ft",
+    "Safe",
+    "to_xml",
+    "highlight",
+    "showtags",
+] + xml_tags
+
+# %% ../nbs/11_xml.ipynb
 
 
-# Dynamically create tags using globals()
-for tag in __all__:
-    globals()[tag] = partial(XMLTag, tag.lower())
+# %% ../nbs/11_xml.ipynb
+def attrmap(o):
+    return o.lstrip("_").replace("_", "-")
+
+
+def valmap(o):
+    return o if isinstance(o, str) else " ".join(map(str, o))
+
+
+def _flatten_tuple(tup):
+    result = []
+    for item in tup:
+        if isinstance(item, tuple):
+            result.extend(item)
+        else:
+            result.append(item)
+    return tuple(result)
+
+
+def _preproc(c, kw, attrmap=attrmap, valmap=valmap):
+    if len(c) == 1 and isinstance(c[0], (types.GeneratorType, map, filter)):
+        c = tuple(c[0])
+    attrs = {attrmap(k.lower()): valmap(v) for k, v in kw.items() if v is not None}
+    return _flatten_tuple(c), attrs
+
+
+# General XML tag creation function
+def ft(
+    tag: str,
+    *c,
+    void_: bool = False,
+    attrmap: callable = attrmap,
+    valmap: callable = valmap,
+    **kw,
+):
+    "Create an `FT` structure for `to_xml()`"
+    return FT(
+        tag.lower(), *_preproc(c, kw, attrmap=attrmap, valmap=valmap), void_=void_
+    )
+
+
+# XML void tags (self-closing)
+voids = set(
+    "area base br col command embed hr img input keygen link meta param source track wbr".split()
+)
+
+# Mapping general XML tags
+_g = globals()
+
+
+# Replace the 'partial' based tag creation
+def create_tag_function(tag, void_):
+    def tag_function(*c, **kwargs):
+        return ft(tag, *c, void_=void_, **kwargs)
+
+    tag_function.__name__ = FT.sanitize_tag_name(tag)  # Assigning sanitized tag name
+    return tag_function
+
+
+# Generate dynamic tag functions and map them to valid Python names
+for tag in xml_tags:
+    sanitized_name = FT.sanitize_tag_name(tag)
+    _g[sanitized_name] = create_tag_function(tag, tag in voids)
+
+
+# %% ../nbs/11_xml.ipynb
+def _escape(s):
+    return escape(s) if isinstance(s, str) else s
+
+
+def _to_attr(k, v):
+    if isinstance(v, bool):
+        return f"{k}" if v else ""
+    return f'{k}="{escape(str(v))}"'
+
+
+def _to_xml(elm, lvl, indent, do_escape):
+    esc_fn = _escape if do_escape else lambda s: s
+    nl = "\n"
+    sp = " " * lvl if indent else ""
+
+    if elm is None:
+        return ""
+
+    if isinstance(elm, tuple):
+        return (
+            f"{nl}".join(
+                _to_xml(o, lvl=lvl, indent=indent, do_escape=do_escape) for o in elm
+            )
+            + nl
+        )
+
+    if not isinstance(elm, FT):
+        return f"{esc_fn(elm)}{nl}"
+
+    tag, cs, attrs = elm.list
+    stag = FT.restore_tag_name(tag)
+
+    # Prepare the attribute string only once
+    attr_str = ""
+    if attrs:
+        attr_str = " " + " ".join(_to_attr(k, v) for k, v in attrs.items())
+
+    # Handle void (self-closing) tags
+    if elm.void_:
+        return f"{sp}<{stag}{attr_str} />{nl}"
+
+    # Handle non-void tags with children or no children
+    if cs:
+        res = f"{sp}<{stag}{attr_str}>{nl if indent else ''}"
+        res += "".join(
+            _to_xml(c, lvl=lvl + 2, indent=indent, do_escape=do_escape) for c in cs
+        )
+        res += f"{sp}</{stag}>{nl}"
+        return Safe(res)
+    else:
+        return f"{sp}<{stag}{attr_str}></{stag}>{nl}"
+
+
+def to_xml(elm, lvl=0, indent: bool = True, do_escape: bool = True):
+    "Convert `ft` element tree into an XML string"
+    return Safe(_to_xml(elm, lvl, indent, do_escape=do_escape))
+
+
+FT.__html__ = to_xml
+
+
+# %% ../nbs/11_xml.ipynb
+class Safe(str):
+    def __html__(self):
+        return self
+
+
+# %% ../nbs/11_xml.ipynb
+def highlight(s, lang="xml"):
+    "Markdown to syntax-highlight `s` in language `lang`"
+    return f"```{lang}\n{to_xml(s)}\n```"
+
+
+# %% ../nbs/11_xml.ipynb
+def showtags(s):
+    return f"""<code><pre>{escape(to_xml(s))}</code></pre>"""
+
+
+FT._repr_markdown_ = highlight
+
+
+# %% ../nbs/11_xml.ipynb
+@patch
+def __call__(self: FT, *c, **kw):
+    c, kw = _preproc(c, kw)
+    if c:
+        self = self + c
+    if kw:
+        self.attrs = {**self.attrs, **kw}
+    return self
--- vespa/test_dynamic.py
+++ vespa/test_dynamic.py
@@ -1,90 +1,141 @@
-from dynamic import *
+import unittest
+from lxml import etree
 
-# Construct the XML structure
-xml_structure = Services(
-    version="1.0", xmlns_deploy="vespa", xmlns_preprocess="properties"
-)(
-    Admin(version="2.0")(
-        Configservers(
-            Configserver(hostalias="node0"),
-            Configserver(hostalias="node1"),
-            Configserver(hostalias="node2"),
-        ),
-        Clustercontrollers(
-            Clustercontroller(hostalias="node0", jvm_options="-Xms32M -Xmx64M"),
-            Clustercontroller(hostalias="node1", jvm_options="-Xms32M -Xmx64M"),
-            Clustercontroller(hostalias="node2", jvm_options="-Xms32M -Xmx64M"),
-        ),
-        Slobroks(
-            Slobrok(hostalias="node0"),
-            Slobrok(hostalias="node1"),
-            Slobrok(hostalias="node2"),
-        ),
-        Adminserver(hostalias="node3"),
-    ),
-    Container(id="feed", version="1.0")(
-        Documentapi(),
-        Documentprocessing(),
-        Nodes(
-            Jvm(options="-Xms32M -Xmx128M"),
-            Node(hostalias="node4"),
-            Node(hostalias="node5"),
-        ),
-    ),
-    Container(id="query", version="1.0")(
-        Search(),
-        Nodes(
-            Jvm(options="-Xms32M -Xmx128M"),
-            Node(hostalias="node6"),
-            Node(hostalias="node7"),
-        ),
-    ),
-    Content(id="music", version="1.0")(
-        Minredundancy("2"),
-        Documents(
-            Document(type="music", mode="index"),
-            Documentprocessing(cluster="feed"),
-        ),
-        Nodes(
-            Node(hostalias="node8", distribution_key="0"),
-            Node(hostalias="node9", distribution_key="1"),
-        ),
-    ),
-)
+from .dynamic import FT, to_xml, attrmap, valmap, create_tag_function, xml_tags, _escape
 
-# Add XML declaration and print the XML string
-xml_output = f"{VT()}\n{xml_structure.to_xml()}"
-print(xml_output)
 
-# Construct the XML structure
-xml_structure = Services(
-    version="1.0", xmlns_deploy="vespa", xmlns_preprocess="properties"
-)(
-    Container(id="default", version="1.0")(
-        Component(id="splade", type="splade-embedder")(
-            Transformermodel(
-                url="https://huggingface.co/Qdrant/Splade_PP_en_v1/resolve/main/model.onnx"
-            ),
-            Tokenizermodel(
-                url="https://huggingface.co/Qdrant/Splade_PP_en_v1/raw/main/tokenizer.json"
-            ),
-            Termscorethreshold("0.8"),
-        ),
-        Documentapi(),
-        Search(),
-        Nodes(count="1")(
-            Resources(vcpu="4", memory="16Gb", disk="125Gb")(
-                Gpu(count="1", memory="16Gb")
-            )
-        ),
-    ),
-    Content(id="text", version="1.0")(
-        Minredundancy("2"),
-        Documents(Document(type="doc", mode="index")),
-        Nodes(count="2"),
-    ),
-)
+class TestXMLSchema(unittest.TestCase):
+    def test_sanitize_tag_name(self):
+        self.assertEqual(FT.sanitize_tag_name("content-node"), "content_node")
+        self.assertEqual(FT.sanitize_tag_name("search-engine"), "search_engine")
 
-# Add XML declaration and print the XML string
-xml_output = f"{VT()}\n{xml_structure.to_xml()}"
-print(xml_output)
+    def test_restore_tag_name(self):
+        self.assertEqual(FT.restore_tag_name("content_node"), "content-node")
+        self.assertEqual(FT.restore_tag_name("search_engine"), "search-engine")
+
+    def test_attrmap(self):
+        self.assertEqual(attrmap("_max_memory"), "max-memory")
+        self.assertEqual(attrmap("content_node"), "content-node")
+
+    def test_valmap(self):
+        self.assertEqual(valmap("test"), "test")
+        self.assertEqual(valmap([1, 2, 3]), "1 2 3")
+
+    def test_single_tag(self):
+        content_tag = FT("content", (), {"attr": "value"})
+        xml_output = to_xml(content_tag, indent=False)
+        # Expecting the compact format without unnecessary newlines
+        self.assertEqual(str(xml_output), '<content attr="value"></content>\n')
+
+    def test_nested_tags(self):
+        nested_tag = FT("content", (FT("document", ()),), {"attr": "value"})
+        xml_output = to_xml(nested_tag, indent=False)
+        # Expecting nested tags with proper newlines and indentation
+        expected_output = '<content attr="value"><document></document></content>\n'
+        self.assertEqual(str(xml_output), expected_output)
+
+    def test_void_tag(self):
+        void_tag = FT("meta", (), void_=True)
+        xml_output = to_xml(void_tag, indent=False)
+        # Expecting a void tag with a newline at the end
+        self.assertEqual(str(xml_output), "<meta />\n")
+
+    def test_tag_with_attributes(self):
+        tag_with_attr = FT(
+            "content", (), {"max-size": "100", "compression-type": "gzip"}
+        )
+        xml_output = to_xml(tag_with_attr, indent=False)
+        # Expecting the tag with attributes in compact format
+        expected_output = '<content max-size="100" compression-type="gzip"></content>\n'
+        self.assertEqual(str(xml_output), expected_output)
+
+    def test_escape(self):
+        unescaped_text = "<content>"
+        self.assertEqual(_escape(unescaped_text), "&lt;content&gt;")
+
+    def test_dynamic_tag_generation(self):
+        for tag in xml_tags:
+            sanitized_name = FT.sanitize_tag_name(tag)
+            tag_func = create_tag_function(tag, False)
+            self.assertEqual(tag_func().__class__, FT)
+            self.assertEqual(tag_func().tag, sanitized_name)
+
+    def test_tag_addition(self):
+        tag = FT("content", (), {"attr": "value"})
+        new_tag = FT("document", ())
+        tag = tag + new_tag
+        self.assertEqual(len(tag.children), 1)
+        self.assertEqual(tag.children[0].tag, "document")
+
+    def test_repr(self):
+        tag = FT("content", (FT("document", ()),), {"attr": "value"})
+        self.assertEqual(repr(tag), "content((document((),{}),),{'attr': 'value'})")
+
+
+class TestColbertSchema(unittest.TestCase):
+    def setUp(self):
+        with open("tests/testfiles/relaxng/services.rng", "rb") as schema_file:
+            self.relaxng = etree.RelaxNG(etree.parse(schema_file))
+        self.xml_schema = """<?xml version="1.0" encoding="utf-8" ?>
+    <services version="1.0" xmlns:deploy="vespa" xmlns:preprocess="properties" minimum-required-vespa-version="8.338.38">
+
+        <!-- See https://docs.vespa.ai/en/reference/services-container.html -->
+        <container id="default" version="1.0">
+
+            <!-- See https://docs.vespa.ai/en/embedding.html#huggingface-embedder -->
+            <component id="e5" type="hugging-face-embedder">
+                <transformer-model url="https://huggingface.co/intfloat/e5-small-v2/resolve/main/model.onnx"/>
+                <tokenizer-model url="https://huggingface.co/intfloat/e5-small-v2/raw/main/tokenizer.json"/>
+                <!-- E5 prompt instructions -->
+                <prepend>
+                    <query>query:</query>
+                    <document>passage:</document>
+                </prepend>
+            </component>
+
+            <!-- See https://docs.vespa.ai/en/embedding.html#colbert-embedder -->
+            <component id="colbert" type="colbert-embedder">
+                <transformer-model url="https://huggingface.co/colbert-ir/colbertv2.0/resolve/main/model.onnx"/>
+                <tokenizer-model url="https://huggingface.co/colbert-ir/colbertv2.0/raw/main/tokenizer.json"/>
+            </component>
+
+            <document-api/>
+            <search/>
+            <nodes count="1">
+                <resources vcpu="4" memory="16Gb" disk="125Gb">
+                    <gpu count="1" memory="16Gb"/>
+                </resources>
+            </nodes>
+            
+        </container>
+
+        <!-- See https://docs.vespa.ai/en/reference/services-content.html -->
+        <content id="text" version="1.0">
+            <min-redundancy>2</min-redundancy>
+            <documents>
+                <document type="doc" mode="index" />
+            </documents>
+            <nodes count="2"/>
+        </content>
+
+    </services>
+    """
+
+    def test_valid_colbert_schema(self):
+        to_validate = etree.parse("tests/testfiles/services/colbert/services.xml")
+        # Validate against relaxng
+        self.relaxng.validate(to_validate)
+        self.assertTrue(self.relaxng.validate(to_validate))
+
+    def test_valid_schema_from_string(self):
+        to_validate = etree.fromstring(self.xml_schema.encode("utf-8"))
+        self.assertTrue(self.relaxng.validate(to_validate))
+
+    def test_invalid_schema_from_string(self):
+        invalid_xml = self.xml_schema.replace("document-api", "document-api-")
+        to_validate = etree.fromstring(invalid_xml.encode("utf-8"))
+        self.assertFalse(self.relaxng.validate(to_validate))
+
+
+if __name__ == "__main__":
+    unittest.main()
