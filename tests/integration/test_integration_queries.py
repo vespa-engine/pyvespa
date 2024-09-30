26e8c1d3bacd7d64a16a61ec41af020d7d8f6793
thomasht86 <thomas@vespa.ai>
2024-09-30 13:08:11 +0200

    use unit tests for integration tests

--- tests/integration/test_integration_queries.py
+++ tests/integration/test_integration_queries.py
@@ -7,8 +7,11 @@ from vespa.package import (
     Field,
     StructField,
     Struct,
+    RankProfile,
 )
-from vespa.querybuilder import Query, Q
+from tests.unit.test_q import TestQueryBuilder
+
+qb = TestQueryBuilder()
 
 
 class TestQueriesIntegration(unittest.TestCase):
@@ -92,76 +95,142 @@ class TestQueriesIntegration(unittest.TestCase):
                 )
             ],
         )
+        rank_profiles = [
+            RankProfile(
+                name="dotproduct",
+                first_phase="rawScore(weightedset_field)",
+                summary_features=["rawScore(weightedset_field)"],
+            ),
+            RankProfile(
+                name="geolocation",
+                first_phase="distance(location_field)",
+                summary_features=["distance(location_field).km"],
+            ),
+        ]
         document = Document(fields=fields, structs=[email_struct])
-        schema = Schema(name=application_name, document=document)
+        schema = Schema(
+            name=application_name, document=document, rank_profiles=rank_profiles
+        )
         schema.add_fields(emails_field)
         application_package = ApplicationPackage(name=application_name, schema=[schema])
         print(application_package.schema.schema_to_text)
         # Deploy the application
         cls.vespa_docker = VespaDocker(port=8089)
         cls.app = cls.vespa_docker.deploy(application_package=application_package)
+        cls.app.wait_for_application_up()
 
     @classmethod
-    def tearDown(cls):
+    def tearDownClass(cls):
         cls.vespa_docker.container.stop(timeout=5)
         cls.vespa_docker.container.remove()
 
+    # @unittest.skip("Skip until we have a better way to test this")
     def test_dotProduct_with_annotations(self):
         # Feed a document with 'weightedset_field'
         field = "weightedset_field"
-        doc = {
-            "id": f"id:{self.application_name}:{self.application_name}::1",
-            "fields": {field: {"feature1": 0.5, "feature2": 1.0}},
-        }
+        fields = {field: {"feature1": 2, "feature2": 4}}
+        data_id = 1
         self.app.feed_data_point(
-            schema=self.application_name, data_id=doc["id"], fields=doc["fields"]
+            schema=self.application_name, data_id=data_id, fields=fields
         )
-
-        # Build and send the query
-        condition = Q.dotProduct(
-            field,
-            {"feature1": 1, "feature2": 2},
-            annotations={"label": "myDotProduct"},
-        )
-        q = (
-            Query(select_fields=[field])
-            .from_(self.application_name)
-            .where(condition)
-            .build(prepend_yql=False)
-        )
-        print(q)
-        result = self.app.query(yql=q)
-
-        # Check the result
+        q = qb.test_dotProduct_with_annotations()
+        with self.app.syncio() as sess:
+            result = sess.query(yql=q, ranking="dotproduct")
+        print(result.json)
         self.assertEqual(len(result.hits), 1)
-        self.assertEqual(result.hits[0]["id"], doc["id"])
+        self.assertEqual(
+            result.hits[0]["id"],
+            f"id:{self.application_name}:{self.application_name}::{data_id}",
+        )
+        self.assertEqual(
+            result.hits[0]["fields"]["summaryfeatures"]["rawScore(weightedset_field)"],
+            10,
+        )
 
     def test_geoLocation_with_annotations(self):
         # Feed a document with 'location_field'
-        doc = {
-            "id": f"id:{self.application_name}:{self.application_name}::2",
-            "fields": {"location_field": "37.7749, -122.4194"},
+        field_name = "location_field"
+        fields = {
+            field_name: {
+                "lat": 37.77491,
+                "lng": -122.41941,
+            },  # 0.00001 degrees more than the query
         }
+        data_id = 2
         self.app.feed_data_point(
-            schema=self.application_name, data_id=doc["id"], fields=doc["fields"]
+            schema=self.application_name, data_id=data_id, fields=fields
         )
-
         # Build and send the query
-        condition = Q.geoLocation(
-            "location_field",
-            37.7749,
-            -122.4194,
-            "10km",
-            annotations={"targetHits": 100},
-        )
-        q = (
-            Query(select_fields="")
-            .from_(self.application_name)
-            .where(condition)
-            .build(prepend_yql=False)
-        )
-        result = self.app.query(yql=q)
-
+        q = qb.test_geoLocation_with_annotations()
+        with self.app.syncio() as sess:
+            result = sess.query(yql=q, ranking="geolocation")
         # Check the result
         self.assertEqual(len(result.hits), 1)
-        self.assertEqual(result.hits[0]["id"], doc["id"])
+        self.assertEqual(
+            result.hits[0]["id"],
+            f"id:{self.application_name}:{self.application_name}::{data_id}",
+        )
+        self.assertAlmostEqual(
+            result.hits[0]["fields"]["summaryfeatures"]["distance(location_field).km"],
+            0.001417364012462494,
+        )
+        print(result.json)
+
+    def test_basic_and_andnot_or_offset_limit_param_order_by_and_contains(self):
+        docs = [
+            {  # Should not match
+                "f1": "v1",
+                "f2": "v2",
+                "f3": "asdf",
+                "f4": "d",
+                "age": 10,
+                "duration": 100,
+            },
+            {  # Should match
+                "f1": "v1",
+                "f2": "v2",
+                "f3": "v3",
+                "f4": "d",
+                "age": 20,
+                "duration": 200,
+            },
+            {  # Should match
+                "f1": "v1",
+                "f2": "v2",
+                "f3": "v3",
+                "f4": "d",
+                "age": 30,
+                "duration": 300,
+            },
+            {  # Should not match
+                "f1": "v1",
+                "f2": "v2",
+                "f3": "v3",
+                "f4": "v4",
+                "age": 30,
+                "duration": 300,
+            },
+        ]
+        id_to_match = 2
+        docs = [
+            {
+                "fields": doc,
+                "id": data_id,
+            }
+            for data_id, doc in enumerate(docs, 1)
+        ]
+        self.app.feed_iterable(iter=docs, schema=self.application_name)
+        # Build and send the query
+        q = qb.test_basic_and_andnot_or_offset_limit_param_order_by_and_contains()
+        print(q)
+        with self.app.syncio() as sess:
+            result = sess.query(
+                yql=q,
+            )
+        # Check the result
+        self.assertEqual(len(result.hits), 1)
+        self.assertEqual(
+            result.hits[0]["id"],
+            f"id:{self.application_name}:{self.application_name}::{id_to_match}",
+        )
+        print(result.json)
