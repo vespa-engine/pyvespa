11c6af7f007bbb76871a299746e5c69524310e74
thomasht86 <thomas@vespa.ai>
2025-08-01 15:24:56 +0200

    add queryprofile config and unit tests

---
+++ vespa/configuration/query_profiles.py
@@ -0,0 +1,22 @@
+from vespa.configuration.vt import VT, create_tag_function, voids
+
+queryprofile_tags = [
+    "query-profile",
+    "query-profile-type",
+    "field",
+    "match",
+    "strict",
+    "description",
+    "dimensions",
+]
+# Fail if any tag is duplicated. Provide feedback of which tags are duplicated.
+duplicated = set([tag for tag in queryprofile_tags if queryprofile_tags.count(tag) > 1])
+if duplicated:
+    raise ValueError(f"Tags duplicated in queryprofile_tags: {duplicated}")
+# Mapping general XML tags
+_g = globals()
+
+# Generate dynamic tag functions and map them to valid Python names
+for tag in queryprofile_tags:
+    sanitized_name = VT.sanitize_tag_name(tag)
+    _g[sanitized_name] = create_tag_function(tag, tag in voids)
