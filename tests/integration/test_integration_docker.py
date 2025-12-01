906b704d4086b5f75d35f8672c311a005893025d
thomasht86 <thomas@vespa.ai>
2025-12-01 16:27:09 +0100

    add integration test

--- tests/integration/test_integration_docker.py
+++ tests/integration/test_integration_docker.py
@@ -1301,6 +1301,21 @@ class TestQaApplication(TestApplicationCommon):
     def test_async_client_accept_encoding(self):
         asyncio.run(self.async_client_accept_encoding_gzip(app=self.app))
 
+    def test_profile_query_option(self):
+        resp = self.app.query(
+            yql="select * from sources * where id contains '1' limit 1;", profile=True
+        )
+        # assert that json response contains timing information
+        self.assertIn("timing", resp.json.keys())
+        # assert that summaryfetchtime, searchtime and querytime is in timing
+        self.assertIn("summaryfetchtime", resp.json["timing"].keys())
+        self.assertIn("searchtime", resp.json["timing"].keys())
+        self.assertIn("querytime", resp.json["timing"].keys())
+        # Assert that json response as string contains profile information
+        resp_raw_string = json.dumps(resp.json)
+        self.assertIn('depth": 100,', resp_raw_string)
+        self.assertIn("timestamp_ms", resp_raw_string)
+
     def tearDown(self) -> None:
         self.vespa_docker.container.stop(timeout=CONTAINER_STOP_TIMEOUT)
         self.vespa_docker.container.remove()
